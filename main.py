87a8eb0c7da9cf84ee4e590c709399f33e812701
plnvsr <pelinavsar98@gmail.com>
2024-02-20 18:35:55 +0300

    edit main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def on_open(ws):
 
 if __name__ == "__main__":
     websocket.enableTrace(True)
-    ws = websocket.WebSocketApp("wss://stream.binance.com:9443/ws",
+    ws = websocket.WebSocketApp("wss://stream.binance.us:9443/ws",
                                 on_message = on_message,
                                 on_error = on_error,
                                 on_close = on_close,
